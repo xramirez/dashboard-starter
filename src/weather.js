5477a303b532f04fcee57729461660316acaeddd
Xavier  Ramirez <xavierramirez@Xaviers-MacBook-Pro.local>
2021-11-24 11:01:05 -0600

    final commit!

--- src/weather.js
+++ src/weather.js
@@ -3,12 +3,43 @@ class Weather {
     lat = 29.9956188;
     long = -95.6519160;
 
-    kelToFar(temp)
-    {
+    cypress = {
+        city: "Cypress, TX",
+        lat: 29.9956188,
+        long: -95.6519160
+    }
+
+    austin = {
+        city: "Austin, TX",
+        lat: 30.2666,
+        long: -97.7333
+    }
+
+    chicago = {
+        city: "Chicago, IL",
+        lat: 41.881832,
+        long: -87.623177
+    }
+
+    london = {
+        city: "London, UK",
+        lat: 51.509865,
+        long: -0.118092
+    }
+
+    giveWeather(city) {
+        return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.long}&exclude=alerts&appid=${this.api_key}`)
+    }
+
+    kelToFar(temp) {
         return Math.floor(((temp) - 273.15) * (9 / 5) + 32)
     }
 
-    render() {
+
+
+    async render() {
+        let cities = [this.cypress, this.austin, this.chicago, this.london]
+        let cityCount = 0;
         let tomo = new Date();
         tomo.setDate(tomo.getDate() + 1);
         let twoDay = new Date();
@@ -16,20 +47,28 @@ class Weather {
         let threeDay = new Date();
         threeDay.setDate(threeDay.getDate() + 3);
 
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${this.lat}&lon=${this.long}&exclude=alerts&appid=${this.api_key}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                //console.log(`Current Temp: ${Math.floor(((data.current.temp) - 273.15) * (9 / 5) + 32)} and ${data.current.weather[0].description}`);
-                let pane4 = document.querySelector('.pane3');
-                pane4.innerHTML = `<h1>${this.kelToFar(data.current.temp)} <i class="fas fa-cloud" style="color:lightgray"></i></i></h1>
+        let resp = await this.giveWeather(cities[cityCount]);
+        let data = await resp.json();
+        console.log(data);
+        let pane4 = document.querySelector('.pane3');
+        pane4.innerHTML = `<h1>${cities[cityCount % 4].city}</h1><h1>${this.kelToFar(data.current.temp)}&#176;</h1>
                 <p class="weather-list"><i class="fas fa-cloud-rain"></i> ${data.hourly[0].pop * 100}% <br>High: ${this.kelToFar(data.daily[0].temp.max)}, Low: ${this.kelToFar(data.daily[0].temp.min)}
                 <br><span class="date">${tomo.getUTCMonth() + 1}/${tomo.getUTCDate()}</span>&emsp; <i class="fas fa-cloud-rain"></i> ${data.daily[1].pop * 100}%&emsp; High: ${this.kelToFar(data.daily[1].temp.max)}, Low: ${this.kelToFar(data.daily[1].temp.min)}
                 <br><span class="date">${twoDay.getUTCMonth() + 1}/${twoDay.getUTCDate()}</span>&emsp; <i class="fas fa-cloud-rain"></i> ${data.daily[2].pop * 100}%&emsp; High: ${this.kelToFar(data.daily[2].temp.max)}, Low: ${this.kelToFar(data.daily[2].temp.min)}
                 <br><span class="date">${threeDay.getUTCMonth() + 1}/${threeDay.getUTCDate()}</span>&emsp; <i class="fas fa-cloud-rain"></i> ${data.daily[3].pop * 100}%&emsp; High: ${this.kelToFar(data.daily[3].temp.max)}, Low: ${this.kelToFar(data.daily[3].temp.min)}</p>`
+        
+        setInterval(async () => {
+            cityCount++;
+            resp = await this.giveWeather(cities[cityCount % 4])
+            data = await resp.json();
 
-                
-            });
+            let pane4 = document.querySelector('.pane3');
+                pane4.innerHTML = `<h1>${cities[cityCount % 4].city}</h1><h1>${this.kelToFar(data.current.temp)}&#176;</h1>
+                <p class="weather-list"><i class="fas fa-cloud-rain"></i> ${data.hourly[0].pop * 100}% <br>High: ${this.kelToFar(data.daily[0].temp.max)}, Low: ${this.kelToFar(data.daily[0].temp.min)}
+                <br><span class="date">${tomo.getUTCMonth() + 1}/${tomo.getUTCDate()}</span>&emsp; <i class="fas fa-cloud-rain"></i> ${data.daily[1].pop * 100}%&emsp; High: ${this.kelToFar(data.daily[1].temp.max)}, Low: ${this.kelToFar(data.daily[1].temp.min)}
+                <br><span class="date">${twoDay.getUTCMonth() + 1}/${twoDay.getUTCDate()}</span>&emsp; <i class="fas fa-cloud-rain"></i> ${data.daily[2].pop * 100}%&emsp; High: ${this.kelToFar(data.daily[2].temp.max)}, Low: ${this.kelToFar(data.daily[2].temp.min)}
+                <br><span class="date">${threeDay.getUTCMonth() + 1}/${threeDay.getUTCDate()}</span>&emsp; <i class="fas fa-cloud-rain"></i> ${data.daily[3].pop * 100}%&emsp; High: ${this.kelToFar(data.daily[3].temp.max)}, Low: ${this.kelToFar(data.daily[3].temp.min)}</p>`
+        }, 8000)
     }
 }
 
